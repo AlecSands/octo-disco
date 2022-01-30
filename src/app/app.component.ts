31e44223c2181664626348c7e0078fc2960861d9
AlecS <sandsalecv@gmail.com>
2022-01-30 11:58:45 -0700

    added example with pipes for title casing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ import { MDCRipple } from '@material/ripple';
 })
 export class AppComponent {
   title = 'octo-disco';
+  pageTitle = 'a page with many examples for testing';
+  buttonTitle= 'example button component';
 }
