805ff39c69521336d4f38afdf2489caa2be895f2
AlecS <sandsalecv@gmail.com>
2022-01-30 12:48:24 -0700

    Added on init and on changes lifecycle methods to button. Added dom selector within on init cycle for MD button JS.

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MDCRipple } from '@material/ripple';
 
 @Component({
   selector: 'app-button',
@@ -7,15 +8,36 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text = '';
+  count = 0;
+  buttonRipple: any;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.count++;
+    console.log(`on changes running: ${this.count}`)
+    console.log(`input value: ${this.text}`);
   }
 
-  buttonClick() {
-    console.log('button click');
+  ngOnInit(): void {
+    console.log(`on init running`);
+    this.trySelectingDom();
+  }
 
+  buttonClick(): void {
+    console.log('button click');
+  }
+
+  trySelectingDom(): void {
+    try {
+      const buttonSelected = document.querySelector('.mdc-button') as HTMLElement;
+      console.log(buttonSelected);
+      this.buttonRipple = new MDCRipple(buttonSelected);
+
+    } catch (error) {
+      console.error(error);
+
+    }
   }
 
 }
